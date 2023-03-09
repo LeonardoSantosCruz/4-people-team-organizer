912a2a20c65dde7e8c0acf75602b027e675526a6
Leonardo dos Santos Cruz <leo.santos199475@gmail.com>
2023-03-09 16:26:12 -0300

    Render inicial funcionando

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,27 @@
-let teams = [["joão","pedro","Maria","Joana"],["josue","pedrosa","Mariana", "Joaquina"]]
-let lastTeam = teams[teams.length - 1]
-let lastTeamIndex = teams.indexOf(lastTeam)
-teams[0].push("José")
+let teams = [[]]
+
+
 // console.log (teams[0][0])
 // console.log (lastTeamIndex)
 
 let addToTeam = () =>{
-    // let addedPlayer = document.getElementById("playerName").value
-    let addedPlayer = "joao"
-// se o tamanho do ultimo time for menor q 4 adicione nele se não crie um novo time
-    if (lastTeam.length<4){
-        teams[lastTeamIndex].push(addedPlayer)
+    let addedPlayer = document.getElementById("playerName").value.toUpperCase()
+    let lastTeam = teams[teams.length - 1]
+    let lastTeamIndex = teams.indexOf(lastTeam) 
+    if (addedPlayer == ""){
+        alert("Favor insira um nome")
     } else{
-        teams.push([addedPlayer])
+        // se o tamanho do ultimo time for menor q 4 adicione nele se não crie um novo time
+        if (lastTeam.length<4){
+            teams[lastTeamIndex].push(addedPlayer)
+        } else{
+            teams.push([addedPlayer])
+        }
+        // lastTeam = teams[teams.length - 1]
+        renderTable()
+        document.getElementById("playerName").value = ""
     }
-    lastTeam = teams[teams.length - 1]
-    renderTable()
+    
 }
 // addToTeam()
 // console.log(teams)
@@ -23,13 +29,37 @@ let addToTeam = () =>{
 
 let renderTable =()=>{
     let teamsTable = document.getElementById('teamsTable')
-    teamsTable.innerHTML=`
-    <thead> 
-    <tr>
-        <th scope="col">Nome</th>
-        <th scope="col">Ações</th>
-    </tr>
-</thead>
-    `
-    // <th colspan = "2" class="bg-primary">Time 1</th> 
+    teamsTable.innerHTML = ""
+    
+    teams.forEach(team =>{
+        let teamTable = `
+        <thead> 
+        <th colspan="2">
+        time ${teams.indexOf(team)+1}
+        </th>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>`
+        teamsTable.innerHTML += teamTable
+
+        team.forEach(player => {
+            const playerRow=`
+        
+        <tr>
+        
+            <td>${player}</td>
+            <td style="width: fit-content;">
+                <button class="btn btn-info btn-sm" onclick="showEditForm()">Editar</button>
+                
+            </td>
+        </tr>`
+        teamsTable.innerHTML += playerRow
+        })
+        
+       
+    })
+    
 }
+// renderTable()
